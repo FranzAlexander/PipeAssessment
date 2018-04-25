348bb8c1fde8be9f59939319b409527f9683e4be
AlexanderFranz <alexbalcombe4@gmail.com>
2018-04-25 15:54:36 +0800

    PipeAssessment

---
+++ QuizAppClient/QuizAppClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace QuizAppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "QuizPipe", PipeDirection.In))
+            {
+                Console.WriteLine("Waiting For Connection");
+                pipeClient.Connect();
+
+                Console.WriteLine("Connected to Pipe");
+                Console.WriteLine("There are currently {0} pipe server instances open.", pipeClient.NumberOfServerInstances);
+                Console.WriteLine("Current Time is.");
+
+                using (StreamReader sr = new StreamReader(pipeClient))
+                {
+                    // Display the text to the console
+                    string temp;
+                    while ((temp = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine("Received from server: {0}", temp);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ QuizAppServer/QuizAppServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace QuizAppServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting communication process.");
+
+            using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("QuizPipe", PipeDirection.Out))
+            {
+                Console.WriteLine("Connection object has been created.");
+
+                Console.WriteLine("Waiting for connection.");
+
+                pipeServer.WaitForConnection();
+
+                Console.WriteLine("Connection Complete.");
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(pipeServer))
+                    {
+                        writer.AutoFlush = true;
+                        writer.WriteLine(DateTime.Now.ToString("h:mm:ss"));
+                    }
+                }
+                catch (IOException e) 
+                {
+                    Console.WriteLine("ERROR: {0}", e.Message);
+                }
+            }
+        }
+    }
+}
